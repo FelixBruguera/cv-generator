52e2af957ce016936485aabacd8249044f7b42f2
Felix Bruguera <felixbruguera0@gmail.com>
2025-03-12 13:27:06 -0400

    Finish the components and add more styling

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,40 @@ function App() {
   const [name, setName] = useState('')
   const [position, setPosition] = useState('')
   const [phone, setPhone] = useState('')
-  
+  const [education, setEducation] = useState([''])
+  const [experience, setExperience] = useState([''])
+  const [isSaved, setIsSaved] = useState(false)
 
-  return (
-    <form>
-      <Input className="title" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}/>
-      <div className="flex">
-        <Input className="subtitle" placeholder="Position" value={position} onChange={(e) => setPosition(e.target.value)}/>
-        <Input className="subtitle" placeholder="Phone Number" value={phone} onChange={(e) => setPhone(e.target.value)}/>
+  const handleSubmission = (e) => {
+    e.preventDefault()
+    setIsSaved(true)
+  }
+  
+  if (isSaved) {
+    return (
+      <div id="cv">
+        <h1 className="title">{name}</h1>
+        <div className="subtitles">
+          <h3 className='subtitle'>{position}</h3>
+          <h3 className='subtitle'>{phone}</h3>
+        </div>
+        <Section title="Education" values={education} setValues={setEducation} isSaved={isSaved}/>
+        <Section title="Experience" values={experience} setValues={setExperience} isSaved={isSaved}/>
       </div>
-      <Section title="Education"/>
-      <Section title="Experience"/>
+    )
+  }
+  return (
+    <form onSubmit={(e) => handleSubmission(e)}>
+      <h2>Personal Information</h2>
+      <input value={name} placeholder="Name" className="text" onChange={(e) => setName(e.target.value)} required></input>
+      <div className="subtitles">
+        <input value={position} placeholder="Position" className="text" onChange={(e) => setPosition(e.target.value)} required></input>
+        <input value={phone} placeholder="Phone" className="text" onChange={(e) => setPhone(e.target.value)} required></input>
+      </div>
+      <Section title="Education" values={education} setValues={setEducation} isSaved={isSaved}/>
+      <Section title="Experience" values={experience} setValues={setExperience} isSaved={isSaved}/>
+      <button type='submit' className='submit'>Save</button>
     </form>
-  )
-}
+)}
 
 export default App
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,24 +1,50 @@
-import { useState } from "react"
-import Input from "./Input"
+const Section = ({ title, values, setValues, isSaved }) => {
+    const handleInput = (e) => {
+        const index = e.target.dataset.index
+        const newValues = [...values]
+        newValues[index] = e.target.value
+        setValues(newValues)
+    }
+    const add = () => {
+        setValues(prev => prev.concat(''))
+    }
+    const remove = (e) => {
+        const index = e.target.dataset.index
+        const newValues = [...values]
+        newValues.splice(index, 1)
+        setValues(newValues)
+    }
 
-const Section = ({ title }) => {
-    const [values, setValues] = useState([''])
+    if (isSaved) {
+        return (
+            <section>
+                <div className="flex">
+                    <h2 className="section-title">{title}</h2>
+                </div>
+                <ul>
+                    {values.map((value) => <li className="text" key={value}>{value}</li>
+                    )}
+                </ul>
+            </section>
+        )
+    }
 
     return (
-    <div className="section">
-        <div className="flex">
-            <h2>{title}</h2>
-            {values.length < 5 ? <button className="add">+</button> : null }
-        </div>
-        {values.map((value, i) => { 
-            return (
-                <div className="flex">
-                    <Input value={value} className="text" data-index={i}/>
-                    <button className="remove">-</button>
-                </div>
-            )
-        })}
-    </div>
+        <section>
+            <div className="flex">
+                <h2>{title}</h2>
+                {values.length < 5 ? 
+                <button className="add" type="button" onClick={add}>+</button> : null }
+            </div>
+            {values.map((value, i) => { 
+                return (
+                    <div className="flex">
+                        <input value={value} className="text" data-index={i} onChange={handleInput} required></input>
+                        <button className="remove" type="button" data-index={i} onClick={remove}>-</button>
+                    </div>
+                )
+            })}
+        </section>
     )
 }
 
